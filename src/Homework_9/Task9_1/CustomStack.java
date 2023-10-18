3530bfd482603b624be185bd71b7954383fa37e0
Igor Stalnoy <7262736@gmail.com>
2023-10-18 19:21:31 +0300

    fix issues for homework #9

--- src/Homework_9/Task9_1/CustomStack.java
+++ src/Homework_9/Task9_1/CustomStack.java
@@ -2,75 +2,71 @@ package Homework_9.Task9_1;
 
 public class CustomStack<T extends Comparable<T>> {
 
-    private CustomStackElement<T> firstElement = null;
-    private CustomStackElement<T> maxElement = null;
+    private CustomStackElement<T> stackPointer = null;
+    private CustomStackElement<T> max = null;
 
-    private static class CustomStackElement<T extends Comparable<T>> implements Comparable<T> {
+    private static class CustomStackElement<T extends Comparable<T>> {
 
-        private final T stackElementLink;
-        private CustomStackElement<T> nextElementLink = null;
-        private CustomStackElement<T> previousMaxElementLink;
+        private final T stackElement;
+        private CustomStackElement<T> next = null;
+        private CustomStackElement<T> previousMax;
 
         public CustomStackElement(T element) {
-            this.stackElementLink = element;
+            this.stackElement = element;
         }
 
         @Override
         public String toString() {
-            return "" + stackElementLink;
+            return "" + stackElement;
         }
 
-        @Override
-        public int compareTo(T element) {
-            return stackElementLink.compareTo(element);
-        }
+
     }
 
     public void push(T element) {
         CustomStackElement<T> temp = new CustomStackElement<>(element);
         if (isStackEmpty()) {
-            maxElement = temp;
-        }
-        if (!isStackEmpty()) {
-            temp.previousMaxElementLink = maxElement;
-            temp.nextElementLink = firstElement;
-            if (temp.compareTo(maxElement.stackElementLink) > 0) {
-                maxElement = temp;
+            max = temp;
+        } else {
+            temp.previousMax = max;
+            temp.next = stackPointer;
+            if (temp.stackElement.compareTo(max.stackElement) > 0) {
+                max = temp;
             }
         }
-        firstElement = temp;
+        stackPointer = temp;
     }
 
-    public CustomStackElement<T> pop() {
+    public T pop() {
         if (isStackEmpty()) {
             return null;
         } else {
-            CustomStackElement<T> temp = firstElement;
-            maxElement = temp.previousMaxElementLink;
-            firstElement = firstElement.nextElementLink;
-            return temp;
+            CustomStackElement<T> temp = stackPointer;
+            max = temp.previousMax;
+            stackPointer = stackPointer.next;
+            return temp.stackElement;
         }
     }
 
     private boolean isStackEmpty() {
-        return firstElement == null;
+        return stackPointer == null;
     }
 
     @Override
     public String toString() {
-        CustomStackElement<T> temp = firstElement;
+        CustomStackElement<T> temp = stackPointer;
         String result = "";
         if (!isStackEmpty()) {
             while (temp != null) {
                 result = result.concat(temp.toString()) + '\n';
-                temp = temp.nextElementLink;
+                temp = temp.next;
             }
         }
         return result;
     }
 
-    public CustomStackElement<T> getMaxElement() {
-        return maxElement;
+    public T getMaxElement() {
+        return max.stackElement;
     }
 }
 
